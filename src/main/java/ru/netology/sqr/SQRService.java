beb7a205f4ab87805228a776e23e6aa648be385f
Darya9810 <93486920+Darya9810@users.noreply.github.com>
2021-12-15 23:28:14 +0300

    Modification

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,15 +2,40 @@ package ru.netology.sqr;
 
 public class SQRService {
 
-    public static int calculateNumberOfSguares(int args) {
+    public static int shouldMinimumNumberOfSquares(int args) {
         int counter = 0;
         int j;
-        for (int i = 10; i <=99; i++) {
-            j = i*i;
-           if (j>=100 && j<=200)
-               counter++;
+        for (int i = 10; i <= 99; i++) {
+            j = i * i;
+            if (j >= 100 && j <= 200)
+                counter++;
         }
         System.out.println(counter);
         return counter;
     }
+
+    public static int shouldAverageNumberOfSquares(int args) {
+        int counter = 0;
+        int j;
+        for (int i = 10; i <= 99; i++) {
+            j = i * i;
+            if (j >= 2000 && j <= 2500)
+                counter++;
+        }
+        System.out.println(counter);
+        return counter;
+    }
+
+    public static int shouldMaximumNumberOfSquares(int args) {
+        int counter = 0;
+        int j;
+        for (int i = 10; i <= 99; i++) {
+            j = i * i;
+            if (j >= 9500 && j <= 9850)
+                counter++;
+        }
+        System.out.println(counter);
+        return counter;
+    }
+
 }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -7,17 +7,38 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @org.junit.jupiter.api.Test
-    void calculateSum() {
-            SQRService SQRService = new SQRService();
+    void shouldMinimumNumberOfSquares() {
+        SQRService sqrService = new SQRService();
 
-            int i = 10;
+        int i = 10;
         int expected = 5;
 
-            int actual = SQRService.calculateNumberOfSguares(i);
+        int actual = SQRService.shouldMinimumNumberOfSquares(i);
 
-            assertEquals(expected, actual);
-        }
-
-
-
+        assertEquals(expected, actual);
     }
+
+    @org.junit.jupiter.api.Test
+    void shouldAverageNumberOfSquares() {
+        SQRService sqrService = new SQRService();
+
+        int i = 10;
+        int expected = 5;
+
+        int actual = SQRService.shouldAverageNumberOfSquares(i);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldMaximumNumberOfSquares() {
+        SQRService sqrService = new SQRService();
+
+        int i = 10;
+        int expected = 5;
+
+        int actual = SQRService.shouldMaximumNumberOfSquares(i);
+
+        assertEquals(expected, actual);
+    }
+}
